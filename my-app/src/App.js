ced88a11fd8a6937e188282dfadd9ddb1f0bc273
ray__s <Rayy_Bush@proton.me>
2025-04-05 22:35:48 +0300

    react_23_2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 function App() {
 	const handleClick = event => {
-		console.log(event) // Выводим объект события в консоль
+		console.log(event.target) // Выводим элемент, на который был совершен клик
 	}
 
 	return <button onClick={handleClick}>Click me</button>
