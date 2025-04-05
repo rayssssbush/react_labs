d0b7bc84f2cc1b06c8af64627fc865b7577fa940
ray__s <Rayy_Bush@proton.me>
2025-04-05 22:22:06 +0300

    react_9

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,15 @@
 function App() {
-	const attr = 'block'
+	const li1 = <li>text1</li>
+	const li2 = <li>text2</li>
+	const li3 = <li>text3</li>
 
-	return <div id={attr}>text</div>
+	return (
+		<ul>
+			{li1}
+			{li2}
+			{li3}
+		</ul>
+	)
 }
 
 export default App
