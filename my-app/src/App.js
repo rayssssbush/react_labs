3dcbf895a40970b6f570e7491e1ebfe15c00195e
ray__s <Rayy_Bush@proton.me>
2025-04-06 00:05:43 +0300

    react_46

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
-	// Массив городов
-	const cities = [
-		'Москва',
-		'Санкт-Петербург',
-		'Новосибирск',
-		'Екатеринбург',
-		'Казань',
-	]
+	const [ageGroup, setAgeGroup] = useState('')
+
+	const handleChange = event => {
+		setAgeGroup(event.target.value)
+	}
 
 	return (
 		<div>
 			<label>
-				Выберите город:
-				<select>
-					<option value=''>Выберите город</option>
-					{/* Используем цикл для отображения городов */}
-					{cities.map((city, index) => (
-						<option key={index} value={city}>
-							{city}
-						</option>
-					))}
+				Выберите вашу возрастную группу:
+				<select value={ageGroup} onChange={handleChange}>
+					<option value=''>Выберите возрастную группу</option>
+					<option value='0-12'>0 - 12 лет</option>
+					<option value='13-17'>13 - 17 лет</option>
+					<option value='18-25'>18 - 25 лет</option>
+					<option value='25+'>старше 25 лет</option>
 				</select>
 			</label>
+
+			<p>Вы выбрали: {ageGroup}</p>
 		</div>
 	)
 }
