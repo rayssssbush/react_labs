0c471f73b1b977e63b30b7291d62fc0b194d286c
ray__s <Rayy_Bush@proton.me>
2025-04-05 23:23:57 +0300

    react_37

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,37 @@
 import React, { useState } from 'react'
 
 function App() {
-	// Стейт для хранения числа (например, возраста)
-	const [age, setAge] = useState(25)
+	// Стейты для хранения текста из инпутов
+	const [inputText1, setInputText1] = useState('')
+	const [inputText2, setInputText2] = useState('')
 
-	// Функции для изменения значения
-	const increaseAge = () => {
-		setAge(age + 1)
+	// Функции для обновления стейтов
+	const handleInput1Change = event => {
+		setInputText1(event.target.value)
 	}
 
-	const decreaseAge = () => {
-		setAge(age - 1)
+	const handleInput2Change = event => {
+		setInputText2(event.target.value)
 	}
 
 	return (
 		<div>
-			<h1>Возраст: {age}</h1>
-
-			{/* Кнопки для увеличения и уменьшения возраста */}
-			<button onClick={increaseAge}>Увеличить возраст</button>
-			<button onClick={decreaseAge}>Уменьшить возраст</button>
+			{/* Первый инпут */}
+			<input
+				type='text'
+				value={inputText1}
+				onChange={handleInput1Change}
+				placeholder='Введите текст 1'
+			/>
+			<p>{inputText1}</p> {/* Вывод текста из первого инпута */}
+			{/* Второй инпут */}
+			<input
+				type='text'
+				value={inputText2}
+				onChange={handleInput2Change}
+				placeholder='Введите текст 2'
+			/>
+			<p>{inputText2}</p> {/* Вывод текста из второго инпута */}
 		</div>
 	)
 }
