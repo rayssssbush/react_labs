63754b219c1b634d65d7952be7986dc34badf157
ray__s <Rayy_Bush@proton.me>
2025-04-05 22:27:24 +0300

    react_14

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,9 @@
 function App() {
+	const isAdult = true // true или false в зависимости от возраста пользователя
+
 	return (
 		<div>
-			<table border='1'>
-				<tr>
-					<td>Row 1, Column 1</td>
-					<td>Row 1, Column 2</td>
-					<td>Row 1, Column 3</td>
-				</tr>
-				<tr>
-					<td>Row 2, Column 1</td>
-					<td>Row 2, Column 2</td>
-					<td>Row 2, Column 3</td>
-				</tr>
-				<tr>
-					<td>Row 3, Column 1</td>
-					<td>Row 3, Column 2</td>
-					<td>Row 3, Column 3</td>
-				</tr>
-			</table>
+			{isAdult ? <p>Вы совершеннолетний!</p> : <p>Вы несовершеннолетний!</p>}
 		</div>
 	)
 }
