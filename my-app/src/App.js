664cb403eeb04ea1ef8656659ed5d0e720b3a657
ray__s <Rayy_Bush@proton.me>
2025-04-06 00:52:44 +0300

    react_72

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,36 @@
 import React from 'react'
 import styled from 'styled-components'
 
-// Стили для контейнера
+// Стилизованный компонент DIVA
+const DIVA = styled.div`
+	width: 150px;
+	height: 150px;
+	background-color: yellow;
+	border: 2px solid black;
+`
+
+// Стилизованный компонент DIVB
+const DIVB = styled.div`
+	width: 150px;
+	height: 150px;
+	background-color: green;
+	border: 3px solid black;
+`
+
+// Контейнер для размещения компонентов
 const Container = styled.div`
 	display: flex;
-	justify-content: space-around;
-	margin: 20px;
+	justify-content: space-between;
 `
 
-// Стили для кнопок с условным рендерингом
-const Button = styled.button`
-	padding: 10px 20px;
-	font-size: 16px;
-	border: none;
-	border-radius: 5px;
-	cursor: pointer;
-
-	background-color: ${props => (props.warn ? 'yellow' : 'green')};
-	color: ${props => (props.warn ? 'red' : 'white')};
-`
-
-function Block() {
+function Block1() {
 	return (
 		<Container>
-			{/* Первая кнопка заблокирована */}
-			<Button disabled>Button 1 (Disabled)</Button>
-
-			{/* Вторая кнопка с пропсом warn */}
-			<Button warn>Button 2 (Warn)</Button>
-
-			{/* Третья кнопка типа reset */}
-			<Button type='reset'>Button 3 (Reset)</Button>
+			<DIVA />
+			<DIVB />
+			<DIVA />
 		</Container>
 	)
 }
 
-export default Block
+export default Block1
