c3b2d36045192721b857d597e7542c5f621dbd98
ray__s <Rayy_Bush@proton.me>
2025-04-06 02:53:58 +0300

    advenced_react_6.2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,20 @@ import { useState } from 'react'
 
 function App() {
 	const [text, setText] = useState('react')
+	const [num, setNum] = useState(0)
 
 	function handleTextClick() {
 		setText(prev => prev + '!')
 	}
 
+	function handleNumClick() {
+		setNum(prev => prev + 1)
+	}
+
 	return (
 		<div>
 			<p onClick={handleTextClick}>{text}</p>
+			<p onClick={handleNumClick}>{num}</p>
 		</div>
 	)
 }
