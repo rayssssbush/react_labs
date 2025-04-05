b86aaa8b25b2f80aaba6c8d449c7df8b657de8ce
ray__s <Rayy_Bush@proton.me>
2025-04-05 23:04:02 +0300

    react_31

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
-import { nanoid } from 'nanoid'
+import { v4 as uuidv4 } from 'react-uuid'
 
 function id() {
-	return nanoid() // Генерация случайного уникального идентификатора
+	return uuidv4() // Генерация уникального идентификатора в формате UUID
 }
 
-console.log(id()) // Например: "V1StGXR8_Z5jdZiL3rxrKZ"
+console.log(id()) // Например: "f47ac10b-58cc-4372-a567-0e02b2c3d479"
