ffa6e1bc08ef5e388398552f61fd870060bd8c1b
ray__s <Rayy_Bush@proton.me>
2025-04-05 22:55:15 +0300

    react_28

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,31 @@
 function App() {
 	const users = [
-		{ name: 'user1', surn: 'surn1', age: 30 },
-		{ name: 'user2', surn: 'surn2', age: 31 },
-		{ name: 'user3', surn: 'surn3', age: 32 },
+		{ id: 1, name: 'user1', surn: 'surn1', age: 30 },
+		{ id: 2, name: 'user2', surn: 'surn2', age: 31 },
+		{ id: 3, name: 'user3', surn: 'surn3', age: 32 },
 	]
 
 	return (
-		<ul>
-			{users.map((user, index) => (
-				<li key={index}>
-					{user.name} {user.surn}, {user.age} years old
-				</li>
-			))}
-		</ul>
+		<table border='1'>
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Name</th>
+					<th>Surname</th>
+					<th>Age</th>
+				</tr>
+			</thead>
+			<tbody>
+				{users.map(user => (
+					<tr key={user.id}>
+						<td>{user.id}</td>
+						<td>{user.name}</td>
+						<td>{user.surn}</td>
+						<td>{user.age}</td>
+					</tr>
+				))}
+			</tbody>
+		</table>
 	)
 }
 
