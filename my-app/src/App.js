65eaef488e3063b57d2fd2749811d9c83441a720
ray__s <Rayy_Bush@proton.me>
2025-04-06 01:10:41 +0300

    react_74

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,14 @@
-import React from 'react';
-import styles from './Inputs.module.css';
+import React from 'react'
+import styles from './Inputs.module.css'
 
 function Inputs() {
-  return (
-    <div>
-      <input className={styles.input} placeholder="Input 1" />
-      <input className={styles.input} placeholder="Input 2" />
-      <input className={styles.input} placeholder="Input 3" />
-    </div>
-  );
+	return (
+		<div className={styles.inputContainer}>
+			<input className={styles.input} placeholder='Input 1' />
+			<input className={styles.input} placeholder='Input 2' />
+			<input className={styles.input} placeholder='Input 3' />
+		</div>
+	)
 }
 
-export default Inputs;
-
-// CSS (Inputs.module.css)
-.input {
-  padding: 10px;
-  font-size: 16px;
-  margin: 10px;
-  border-radius: 5px;
-  border: 1px solid #ccc;
-}
+export default Inputs
