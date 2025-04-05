6b736611f083ca27fd1cb34bd8202d5f5e999972
ray__s <Rayy_Bush@proton.me>
2025-04-05 22:23:09 +0300

    react_11

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,7 @@
 function App() {
-	const li1 = <li>text1</li>
-	const li2 = <li>text2</li>
-	const li3 = <li>text3</li>
+	const items = [<li>text1</li>, <li>text2</li>, <li>text3</li>]
 
-	return (
-		<ul>
-			{li1}
-			{li2}
-			{li3}
-		</ul>
-	)
+	return <ul>{items}</ul>
 }
 
 export default App
