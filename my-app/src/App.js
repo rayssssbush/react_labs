979e63bebabe1b1f5c80c753c4b603c3bf28d4e6
ray__s <Rayy_Bush@proton.me>
2025-04-05 23:58:43 +0300

    react_42

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,113 +1,74 @@
 import React, { useState } from 'react'
 
 function App() {
-	const [text, setText] = useState('')
-	const [sum, setSum] = useState(0)
+	// Для первой задачи
+	const [isChecked, setIsChecked] = useState(false)
+	const [message, setMessage] = useState('')
 
-	// Функция для транслитерации текста
-	const transliterate = input => {
-		const translitMap = {
-			а: 'a',
-			б: 'b',
-			в: 'v',
-			г: 'g',
-			д: 'd',
-			е: 'e',
-			ё: 'yo',
-			ж: 'zh',
-			з: 'z',
-			и: 'i',
-			й: 'y',
-			к: 'k',
-			л: 'l',
-			м: 'm',
-			н: 'n',
-			о: 'o',
-			п: 'p',
-			р: 'r',
-			с: 's',
-			т: 't',
-			у: 'u',
-			ф: 'f',
-			х: 'kh',
-			ц: 'ts',
-			ч: 'ch',
-			ш: 'sh',
-			щ: 'shch',
-			ы: 'y',
-			э: 'e',
-			ю: 'yu',
-			я: 'ya',
-			// Преобразование в верхний регистр
-			А: 'A',
-			Б: 'B',
-			В: 'V',
-			Г: 'G',
-			Д: 'D',
-			Е: 'E',
-			Ё: 'Yo',
-			Ж: 'Zh',
-			З: 'Z',
-			И: 'I',
-			Й: 'Y',
-			К: 'K',
-			Л: 'L',
-			М: 'M',
-			Н: 'N',
-			О: 'O',
-			П: 'P',
-			Р: 'R',
-			С: 'S',
-			Т: 'T',
-			У: 'U',
-			Ф: 'F',
-			Х: 'Kh',
-			Ц: 'Ts',
-			Ч: 'Ch',
-			Ш: 'Sh',
-			Щ: 'Shch',
-			Ы: 'Y',
-			Э: 'E',
-			Ю: 'Yu',
-			Я: 'Ya',
+	// Для второй задачи
+	const [htmlChecked, setHtmlChecked] = useState(false)
+	const [cssChecked, setCssChecked] = useState(false)
+	const [jsChecked, setJsChecked] = useState(false)
+
+	// Обработчик для первой задачи
+	const handleGreeting = () => {
+		if (isChecked) {
+			setMessage('Привет, пользователь!')
+		} else {
+			setMessage('До свидания!')
 		}
-		return input
-			.split('')
-			.map(char => translitMap[char] || char)
-			.join('')
 	}
 
-	// Обработчик для textarea
-	const handleTextChange = e => {
-		setText(e.target.value)
-	}
-
-	// Обработчик для суммирования чисел
-	const handleSumChange = e => {
-		const lines = e.target.value.split('\n')
-		const sum = lines.reduce((acc, line) => acc + (parseFloat(line) || 0), 0)
-		setSum(sum)
-	}
+	// Обработчик для второй задачи
+	const handleHtmlChange = e => setHtmlChecked(e.target.checked)
+	const handleCssChange = e => setCssChecked(e.target.checked)
+	const handleJsChange = e => setJsChecked(e.target.checked)
 
 	return (
 		<div>
-			{/* Задача 1: Транслитерация текста */}
+			{/* Задача 1: Приветствие и прощание по чекбоксу */}
 			<div>
-				<textarea
-					value={text}
-					onChange={handleTextChange}
-					placeholder='Введите текст'
+				<input
+					type='checkbox'
+					checked={isChecked}
+					onChange={e => setIsChecked(e.target.checked)}
 				/>
-				<p>Транслит: {transliterate(text)}</p>
+				<button onClick={handleGreeting}>Показать сообщение</button>
+				<p>{message}</p>
 			</div>
 
-			{/* Задача 2: Суммирование чисел на каждой строке */}
+			{/* Задача 2: Выбор языков */}
 			<div>
-				<textarea
-					onChange={handleSumChange}
-					placeholder='Введите числа на каждой строке'
-				/>
-				<p>Сумма чисел: {sum}</p>
+				<h3>Выберите языки, которые вы знаете:</h3>
+				<label>
+					<input
+						type='checkbox'
+						checked={htmlChecked}
+						onChange={handleHtmlChange}
+					/>
+					HTML
+				</label>
+				<p>{htmlChecked ? 'Вы знаете HTML' : 'Вы не выбрали HTML'}</p>
+
+				<label>
+					<input
+						type='checkbox'
+						checked={cssChecked}
+						onChange={handleCssChange}
+					/>
+					CSS
+				</label>
+				<p>{cssChecked ? 'Вы знаете CSS' : 'Вы не выбрали CSS'}</p>
+
+				<label>
+					<input
+						type='checkbox'
+						checked={jsChecked}
+						onChange={handleJsChange}
+					/>
+					JS
+				</label>
+				<p>{jsChecked ? 'Вы знаете JS' : 'Вы не выбрали JS'}</p>
 			</div>
 		</div>
 	)
