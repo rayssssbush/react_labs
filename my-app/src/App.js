fb2afdefb6b46154dbd0c292d0cb4fcb281ffc35
ray__s <Rayy_Bush@proton.me>
2025-04-06 01:36:00 +0300

    advenced_react_2.1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,26 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App() {
+	const [color, setColor] = useState('white')
+
+	// Обработчик клика для изменения фона
+	function handleClick() {
+		setColor(color === 'white' ? 'lightblue' : 'white') // Меняем фон на светло-голубой или белый
+	}
+
 	useEffect(() => {
-		document.title = 'Новое название страницы'
-	}, []) // Пустой массив зависимостей, чтобы выполнить только один раз после рендера.
+		// Добавляем обработчик кликов на документ
+		document.addEventListener('click', handleClick)
+
+		// Очистка при размонтировании компонента
+		return () => {
+			document.removeEventListener('click', handleClick)
+		}
+	}, [color])
 
 	return (
-		<div>
-			<h1>React App</h1>
+		<div style={{ backgroundColor: color, height: '100vh' }}>
+			<h1>Click anywhere to change background color</h1>
 		</div>
 	)
 }
