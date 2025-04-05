bf3647e848cf4dcd4853dd3757be2e930119105d
ray__s <Rayy_Bush@proton.me>
2025-04-06 01:38:50 +0300

    advenced_react_3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,43 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import { MyContext } from './MyContext' // Импортируем контекст
 
 function App() {
-	const [isVisible, setIsVisible] = useState(false)
-
-	// Обработчик клика для скрытия блока
-	function handleClickOutside(event) {
-		if (!event.target.closest('.popup')) {
-			setIsVisible(false) // Скрываем блок, если клик был вне его
-		}
-	}
-
-	// Очистка и добавление обработчика события
-	useEffect(() => {
-		if (isVisible) {
-			document.addEventListener('click', handleClickOutside)
-		}
-		return () => {
-			document.removeEventListener('click', handleClickOutside)
-		}
-	}, [isVisible])
-
 	return (
 		<div>
-			<a href='#' onClick={() => setIsVisible(true)}>
-				Show Block
-			</a>
-
-			{isVisible && (
-				<div
-					className='popup'
-					style={{
-						padding: '20px',
-						backgroundColor: 'lightgreen',
-						marginTop: '10px',
-					}}
-				>
-					<p>This is a popup! Click anywhere outside to close it.</p>
-				</div>
-			)}
+			<h1>Контекст в React</h1>
+			{/* Здесь будет использоваться контекст */}
 		</div>
 	)
 }
---
+++ my-app/src/MyContext.js
@@ -0,0 +1,4 @@
+import { createContext } from 'react'
+
+// Создаем контекст с начальным значением null
+export const MyContext = createContext(null)
