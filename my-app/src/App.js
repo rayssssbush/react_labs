5dda1a5079039af9296756e8509822199a8b9b33
ray__s <Rayy_Bush@proton.me>
2025-04-05 23:55:52 +0300

    react_39

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,74 @@
 import React, { useState } from 'react'
 
 function App() {
-	// Стейт для хранения температуры в Фаренгейтах
-	const [fahrenheit, setFahrenheit] = useState('')
+	// Стейт для хранения значений инпутов
+	const [values, setValues] = useState({
+		input1: '',
+		input2: '',
+		input3: '',
+		input4: '',
+		input5: '',
+	})
 
-	// Функция для обработки изменения инпута
-	const handleFahrenheitChange = event => {
-		setFahrenheit(event.target.value)
+	// Функция для обработки изменения значений инпутов
+	const handleChange = event => {
+		const { name, value } = event.target
+		setValues(prevValues => ({
+			...prevValues,
+			[name]: value,
+		}))
 	}
 
-	// Конвертация в Цельсий
-	const celsius = fahrenheit ? ((fahrenheit - 32) * 5) / 9 : ''
+	// Функция для вычисления среднего арифметического
+	const getAverage = () => {
+		const nums = Object.values(values)
+			.map(Number)
+			.filter(num => !isNaN(num))
+		const sum = nums.reduce((acc, curr) => acc + curr, 0)
+		return nums.length > 0 ? sum / nums.length : 0
+	}
 
 	return (
 		<div>
-			{/* Инпут для ввода температуры в Фаренгейтах */}
+			{/* Инпуты для ввода чисел */}
 			<input
 				type='number'
-				value={fahrenheit}
-				onChange={handleFahrenheitChange}
-				placeholder='Введите температуру в Фаренгейтах'
+				name='input1'
+				value={values.input1}
+				onChange={handleChange}
+				placeholder='Введите число'
+			/>
+			<input
+				type='number'
+				name='input2'
+				value={values.input2}
+				onChange={handleChange}
+				placeholder='Введите число'
+			/>
+			<input
+				type='number'
+				name='input3'
+				value={values.input3}
+				onChange={handleChange}
+				placeholder='Введите число'
+			/>
+			<input
+				type='number'
+				name='input4'
+				value={values.input4}
+				onChange={handleChange}
+				placeholder='Введите число'
+			/>
+			<input
+				type='number'
+				name='input5'
+				value={values.input5}
+				onChange={handleChange}
+				placeholder='Введите число'
 			/>
 
-			{/* Абзац, показывающий температуру в Цельсиях */}
-			{fahrenheit && <p>Температура в Цельсиях: {celsius.toFixed(2)}</p>}
+			{/* Абзац для вывода среднего арифметического */}
+			<p>Среднее арифметическое: {getAverage()}</p>
 		</div>
 	)
 }
