5c6dc2199a39c25c9e216844f303c5667d704a72
ray__s <Rayy_Bush@proton.me>
2025-04-05 22:14:40 +0300

    react_5

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,5 @@
 function App() {
-	return (
-		<div>
-			<ul>
-				<li>text1</li>
-				<li>text2</li>
-				<li>text3</li>
-			</ul>
-			<ul>
-				<li>text1</li>
-				<li>text2</li>
-				<li>text3</li>
-			</ul>
-		</div>
-	)
+	return <input />
 }
 
 export default App
