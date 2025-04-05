1128f8ae79d2b140ce6d90cb444eacebeed79286
ray__s <Rayy_Bush@proton.me>
2025-04-06 00:16:53 +0300

    react_58

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,52 +1,56 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
-// Компонент Employee
-function Employee({ surname, name, patronymic, salary }) {
+// Функция для генерации уникальных ID (можно заменить на библиотеку)
+function id() {
+	return Math.random().toString(36).substr(2, 9)
+}
+
+// Компонент User
+function User({ name, surn, age }) {
 	return (
-		<div>
-			<h2>Работник</h2>
-			<p>
-				<strong>Фамилия:</strong> {surname}
-			</p>
-			<p>
-				<strong>Имя:</strong> {name}
-			</p>
-			<p>
-				<strong>Отчество:</strong> {patronymic}
-			</p>
-			<p>
-				<strong>Зарплата:</strong> {salary} руб.
-			</p>
-			<hr />
-		</div>
+		<tr>
+			<td>{name}</td>
+			<td>{surn}</td>
+			<td>{age}</td>
+		</tr>
 	)
 }
 
-// Компонент App, который использует несколько экземпляров компонента Employee
+// Массив пользователей
+const users = [
+	{ id: id(), name: 'user1', surn: 'surn1', age: 30 },
+	{ id: id(), name: 'user2', surn: 'surn2', age: 31 },
+	{ id: id(), name: 'user3', surn: 'surn3', age: 32 },
+]
+
+// Компонент App
 function App() {
 	return (
 		<div>
-			<h1>Данные о работниках</h1>
-			<Employee
-				surname='Иванов'
-				name='Иван'
-				patronymic='Иванович'
-				salary='50000'
-			/>
-			<Employee
-				surname='Петров'
-				name='Петр'
-				patronymic='Петрович'
-				salary='60000'
-			/>
-			<Employee
-				surname='Сидоров'
-				name='Сидор'
-				patronymic='Сидорович'
-				salary='55000'
-			/>
+			<h1>Список пользователей</h1>
+			<table border='1'>
+				<thead>
+					<tr>
+						<th>Имя</th>
+						<th>Фамилия</th>
+						<th>Возраст</th>
+					</tr>
+				</thead>
+				<tbody>
+					{users.map(user => (
+						<User
+							key={user.id}
+							name={user.name}
+							surn={user.surn}
+							age={user.age}
+						/>
+					))}
+				</tbody>
+			</table>
 		</div>
 	)
 }
 
-export default App
+// Рендерим компонент App
+ReactDOM.render(<App />, document.getElementById('root'))
