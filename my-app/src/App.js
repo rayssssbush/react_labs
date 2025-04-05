610fe7638b610ae09387abb128facd6228ecb54a
ray__s <Rayy_Bush@proton.me>
2025-04-06 00:06:49 +0300

    react_47

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,46 @@
 import React, { useState } from 'react'
 
 function App() {
-	const [ageGroup, setAgeGroup] = useState('')
+	const [favoriteLanguage, setFavoriteLanguage] = useState('')
 
-	const handleChange = event => {
-		setAgeGroup(event.target.value)
+	const handleLanguageChange = event => {
+		setFavoriteLanguage(event.target.value)
 	}
 
 	return (
 		<div>
 			<label>
-				Выберите вашу возрастную группу:
-				<select value={ageGroup} onChange={handleChange}>
-					<option value=''>Выберите возрастную группу</option>
-					<option value='0-12'>0 - 12 лет</option>
-					<option value='13-17'>13 - 17 лет</option>
-					<option value='18-25'>18 - 25 лет</option>
-					<option value='25+'>старше 25 лет</option>
-				</select>
+				<input
+					type='radio'
+					name='language'
+					value='JavaScript'
+					onChange={handleLanguageChange}
+				/>
+				JavaScript
+			</label>
+			<label>
+				<input
+					type='radio'
+					name='language'
+					value='Python'
+					onChange={handleLanguageChange}
+				/>
+				Python
+			</label>
+			<label>
+				<input
+					type='radio'
+					name='language'
+					value='Java'
+					onChange={handleLanguageChange}
+				/>
+				Java
 			</label>
 
-			<p>Вы выбрали: {ageGroup}</p>
+			<p>Ваш любимый язык программирования: {favoriteLanguage}</p>
+			{favoriteLanguage === 'JavaScript' && (
+				<p>Отличный выбор! JavaScript — это супер!</p>
+			)}
 		</div>
 	)
 }
