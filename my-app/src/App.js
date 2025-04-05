791c476d05798d7d15382f65bae903020f37ffee
ray__s <Rayy_Bush@proton.me>
2025-04-05 22:13:41 +0300

    react_4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,17 @@
-import React from 'react'
-
 function App() {
 	return (
-		<ul>
-			<li>Элемент 1</li>
-			<li>Элемент 2</li>
-			<li>Элемент 3</li>
-			<li>Элемент 4</li>
-			<li>Элемент 5</li>
-			<li>Элемент 6</li>
-			<li>Элемент 7</li>
-			<li>Элемент 8</li>
-			<li>Элемент 9</li>
-			<li>Элемент 10</li>
-		</ul>
+		<div>
+			<ul>
+				<li>text1</li>
+				<li>text2</li>
+				<li>text3</li>
+			</ul>
+			<ul>
+				<li>text1</li>
+				<li>text2</li>
+				<li>text3</li>
+			</ul>
+		</div>
 	)
 }
 
