8f864855c9f89aad64457d87c965cfd6afce4092
ray__s <Rayy_Bush@proton.me>
2025-04-05 22:56:41 +0300

    react_29

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,12 @@
-function App() {
-	const users = [
-		{ id: 1, name: 'user1', surn: 'surn1', age: 30 },
-		{ id: 2, name: 'user2', surn: 'surn2', age: 31 },
-		{ id: 3, name: 'user3', surn: 'surn3', age: 32 },
-	]
+import faker from 'faker'
 
-	return (
-		<table border='1'>
-			<thead>
-				<tr>
-					<th>ID</th>
-					<th>Name</th>
-					<th>Surname</th>
-					<th>Age</th>
-				</tr>
-			</thead>
-			<tbody>
-				{users.map(user => (
-					<tr key={user.id}>
-						<td>{user.id}</td>
-						<td>{user.name}</td>
-						<td>{user.surn}</td>
-						<td>{user.age}</td>
-					</tr>
-				))}
-			</tbody>
-		</table>
-	)
+function generateRandomStringWithFaker() {
+	// Генерация случайной строки с использованием faker
+	const randomName = faker.name.findName()
+	const randomEmail = faker.internet.email()
+
+	console.log('Random Name:', randomName)
+	console.log('Random Email:', randomEmail)
 }
 
-export default App
+generateRandomStringWithFaker()
