27790bdb2a55af0e73d1d689cd27d5be36b88ca6
ray__s <Rayy_Bush@proton.me>
2025-04-06 04:36:00 +0300

    vite_app_12

--- vite_app/my-app/src/routes/root.jsx
+++ vite_app/my-app/src/routes/root.jsx
@@ -1,21 +1,28 @@
-import { useLoaderData, Link, Outlet } from 'react-router-dom'; // Импортируем необходимые компоненты
+import { useLoaderData, Link, Outlet, Form } from 'react-router-dom'; // Импортируем необходимые компоненты
 
 function Root() {
   const { students } = useLoaderData(); // Получаем данные студентов через useLoaderData
 
   return (
     <div id="main">
-      {students.length ? (
-        <nav>
-          {students.map((student) => (
-            <Link key={student.id} to={`students/${student.id}`}>
-              {student.name ? student.name : <i>Unnamed</i>} {/* Выводим имя студента или "Unnamed" */}
-            </Link>
-          ))}
-        </nav>
-      ) : (
-        <p><i>no students here ...</i></p> {/* Если студентов нет */}
-      )}
+      <div id="menu">
+        {/* Кнопка для добавления нового студента */}
+        <Form method="post">
+          <button type="submit">add student</button>
+        </Form>
+
+        {students.length ? (
+          <nav>
+            {students.map((student) => (
+              <Link key={student.id} to={`students/${student.id}`}>
+                {student.name ? student.name : <i>Unnamed</i>} {/* Выводим имя студента или "Unnamed" */}
+              </Link>
+            ))}
+          </nav>
+        ) : (
+          <p><i>no students here ...</i></p> {/* Если студентов нет */}
+        )}
+      </div>
 
       <div id="student">
         <Outlet /> {/* Рендерим дочерние маршруты */}
