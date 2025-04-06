7a2f2eed00a7dfebd4a539cf761df35725db6634
ray__s <Rayy_Bush@proton.me>
2025-04-06 03:24:09 +0300

    vite_app_2

--- vite_app/my-app/src/main.jsx
+++ vite_app/my-app/src/main.jsx
@@ -3,15 +3,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+// Создаем роутер с основным маршрутом
 const router = createBrowserRouter([
 	{
-		path: '/',
-		element: <p>I'm number one in React!</p>,
+		path: '/', // Главная страница
+		element: <p>I'm number one in React!</p>, // Выводим нужный текст
 	},
 ])
 
+// Рендерим приложение
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<RouterProvider router={router} /> {/* Подключаем роутер */}
 	</React.StrictMode>
 )
