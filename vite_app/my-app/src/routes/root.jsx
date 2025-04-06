8f771bbe93f0e48022e5a173e66be516ad3c7921
ray__s <Rayy_Bush@proton.me>
2025-04-06 04:16:42 +0300

    vite_app_8

--- vite_app/my-app/src/routes/root.jsx
+++ vite_app/my-app/src/routes/root.jsx
@@ -1,17 +1,15 @@
 // src/routes/root.jsx
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 function Root() {
 	return (
 		<div id='main'>
 			<nav>
-				<a href={`/students/1`}>Student1</a>
-				<a href={`/students/2`}>Student2</a>
-				<a href={`/products/1`}>Product1</a>
-				<a href={`/products/2`}>Product2</a>
+				<Link to={`/products/1`}>Product1</Link>
+				<Link to={`/products/2`}>Product2</Link>
 			</nav>
 			<div id='content'>
-				<Outlet /> {/* Здесь будут отображаться вложенные маршруты */}
+				<Outlet /> {/* Отображение дочерних маршрутов */}
 			</div>
 		</div>
 	)
