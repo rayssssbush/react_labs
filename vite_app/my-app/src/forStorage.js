76987602e9650c2df37be6a65a35df8dda2c6275
ray__s <Rayy_Bush@proton.me>
2025-04-06 04:37:50 +0300

    vite_app_13

--- vite_app/my-app/src/forStorage.js
+++ vite_app/my-app/src/forStorage.js
@@ -1,35 +1,45 @@
-// src/forStorage.js
 import localforage from 'localforage'
+import { nanoid } from 'nanoid' // Импортируем библиотеку nanoid для генерации уникальных id
 
-// Функция для получения продуктов из хранилища
-export async function getProducts() {
-	await someNetwork()
-	let products = await localforage.getItem('products')
-	if (!products) products = [] // Если продуктов нет, создаем пустой массив
-	return products
-}
-// Функция для получения студентов из хранилища
+// Функция для получения списка студентов из хранилища
 export async function getStudents() {
 	await someNetwork()
 	let students = await localforage.getItem('students')
-	if (!students) students = [] // Если студентов нет, создаем пустой массив
+	if (!students) students = []
 	return students
 }
-// Имитация задержки для сети
+
+// Функция для создания нового студента
+export async function createStudent() {
+	await someNetwork()
+	let id = nanoid(6) // Генерация уникального id для студента
+	let student = { id } // Новый студент с уникальным id
+	let students = await getStudents() // Получаем текущий список студентов
+	students.unshift(student) // Добавляем студента в начало списка
+	await setStudents(students) // Обновляем список студентов в хранилище
+	return student // Возвращаем новый объект студента
+}
+
+// Функция для обновления списка студентов в хранилище
+function setStudents(students) {
+	return localforage.setItem('students', students) // Сохраняем новый список студентов в хранилище
+}
+
+// Для имитации задержки при сетевых запросах
 let someCache = {}
 
 async function someNetwork(key) {
 	if (!key) {
-		someCache = {} // Очищаем кеш
+		someCache = {}
 	}
 
 	if (someCache[key]) {
-		return // Если кеширован, возвращаем без задержки
+		return
 	}
 
 	someCache[key] = true
 
 	return new Promise(res => {
-		setTimeout(res, Math.random() * 700) // Имитация задержки до 0.7 секунды
+		setTimeout(res, Math.random() * 700) // Имитация задержки
 	})
 }
