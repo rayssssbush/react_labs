0441970aae60be470c71d8b77566960fba67713e
ray__s <Rayy_Bush@proton.me>
2025-04-06 03:23:45 +0300

    vite_app_1

---
+++ vite_app/my-app/src/main.jsx
@@ -0,0 +1,17 @@
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <p>I'm number one in React!</p>,
+	},
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+)
